0cc64dc12f522aff203cc546f250f87456a61412
Thiago Fontanella <thiago.fontanella@hotmail.com>
2023-12-13 22:54:47 -0300

    chore: refactoring handle click

--- main.js
+++ main.js
@@ -3,20 +3,18 @@ let count = 1;
 
 function handleClick (event) {
     event.preventDefault()
-    
     const inputNumber = document.querySelector('input#number-guess')
-    if(Number(inputNumber.value) !== randomNumber) {
-        count++
-        alert('Errou. Tente novamente.')
-    } else {
+
+    if(Number(inputNumber.value) === randomNumber) {
         let guessContainer = document.querySelector(".guess-container")
         let resultContainer = document.querySelector(".result-container")
         let resultContainerText = document.querySelector(".result-container h2")
         let innerResultText = `You got it right in ${count} tries`
 
-
         guessContainer.classList.toggle('hide')
         resultContainerText.innerText = innerResultText;
         resultContainer.classList.toggle('hide')
-    }
+    } 
+
+    count++
 }
