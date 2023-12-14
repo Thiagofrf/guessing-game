525a2bbe8f07a186d2582ffe472ca5bfc9c1bb83
Thiago Fontanella <thiago.fontanella@hotmail.com>
2023-12-13 23:29:27 -0300

    feature: refactoring and adding new functions

--- main.js
+++ main.js
@@ -1,25 +1,31 @@
 let randomNumber = Math.floor(Math.random() * 11);
 let count = 1;
-let guessContainer = document.querySelector(".guess-container")
-let resultContainer = document.querySelector(".result-container")
+const guessContainer = document.querySelector(".guess-container")
+const resultContainer = document.querySelector(".result-container")
+const btnTry = guessContainer.querySelector('button')
+const btnPlayAgain = resultContainer.querySelector('button')
 
-const btnTry = document.querySelector('.guess-container button')
 btnTry.addEventListener('click', handleTryClick)
-
-const btnPlayAgain = document.querySelector('.result-container button')
 btnPlayAgain.addEventListener('click', handlePlayAgain)
+document.addEventListener('keypress', function (e) {
+    if(e.key == 'Enter' && guessContainer.classList.contains('hide')) {
+        handlePlayAgain()
+    }
+})
 
 function handleTryClick (event) {
     event.preventDefault()
     const inputNumber = document.querySelector('input#number-guess')
 
-    if(Number(inputNumber.value) === randomNumber) {
-        let resultContainerText = resultContainer.querySelector("h2")
-        let innerResultText = `You got it right in ${count} tries`
+    if(Number(inputNumber.value) < 0 || Number(inputNumber.value) > 10 || inputNumber.value == '') {
+        inputNumber.value = '';
+        alert('Type a number between 0 to 10. Try again.')
+        count--
+    }
 
-        guessContainer.classList.toggle('hide')
-        resultContainerText.innerText = innerResultText;
-        resultContainer.classList.toggle('hide')
+    if(Number(inputNumber.value) === randomNumber) { 
+        resultContainer.querySelector("h2").innerText = `You got it right in ${count} tries`;
+        toggleScreen()
     } 
 
     inputNumber.value = ""
@@ -29,6 +35,10 @@ function handleTryClick (event) {
 function handlePlayAgain () {
     randomNumber = Math.floor(Math.random() * 11);
     count = 1;
+    toggleScreen()
+}
+
+function toggleScreen () {
     guessContainer.classList.toggle('hide')
     resultContainer.classList.toggle('hide')
 }
